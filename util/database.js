c4066cb3e5e1b7e419b67d4c0304c3dac9b34a20
danielyoo98 <dyoo9@my.bcit.ca>
2020-03-15 21:06:43 -0700

    changed database config

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  database: 'artist',
-  password: 'password',
+  host: 'ec2-3-229-210-93.compute-1.amazonaws.com',
+  user: 'ikzlbxigjmdbqi',
+  database: 'ddd3areasld2fi',
+  password: 'c14d79586a873c152ba3c0d273e962d3d299193f0c09b244bcba0b74701009f1',
   port: 5432
 });
 
